c5f560074bbee64c86858a1ae21e89896e0df5e0
Nosilv <nosilv8@gmail.com>
2025-02-27 03:33:02 +0900

    레벨 3. PasswordEncoder, Test 코드 분석

--- src/main/java/org/example/expert/config/PasswordEncoder.java
+++ src/main/java/org/example/expert/config/PasswordEncoder.java
@@ -10,6 +10,7 @@ public class PasswordEncoder {
         return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
     }
 
+    // 첫번째 인자를 BCrypt 해싱 알고리즘을 사용하여 암호화한 후, 두번째 인자(해시값)과 비교하는 메소드
     public boolean matches(String rawPassword, String encodedPassword) {
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
