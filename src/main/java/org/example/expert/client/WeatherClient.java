6b560a33088a2ebe44a104f6227cc44f3e513f77
Nosilv <nosilv8@gmail.com>
2025-02-26 20:18:30 +0900

    레벨 1-2: 리팩토링 퀴즈 - 불필요한  피하기

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -23,7 +23,7 @@ public class WeatherClient {
     }
 
     // 외부 API를 사용하여 오늘의 날씨 데이터를 가져와서 weatherDto에 맞추어 반환하는 메소드
-    // 예외 : 1. API 요청자체가 실패한 경우, 2. 오늘의 날씨 데이터가 없는 경우
+    // 예외 : 1. API 요청자체가 실패한 경우, 2. 날씨 데이터가 빈 경우(null or 0개) 3. 오늘의 날씨 데이터가 없는 경우
     public String getTodayWeather() {
 
         // buildWeatherApiUri(): 요청할 날씨API URL을 생성하여
@@ -33,21 +33,22 @@ public class WeatherClient {
         ResponseEntity<WeatherDto[]> responseEntity =
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
 
-        // 위에서 할당받은 값(responseEntity)의 Body 만을 WeatherDto 배열에 할당하기
-        WeatherDto[] weatherArray = responseEntity.getBody();
-
         // 위에서 할당받은 값(responseEntity)의 상태코드가 정상OK(200)인지 체크하여 정상이 아니면 ServerException 발생
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
-        // 위에서 할당받은 값의 상태코드가 정상이긴하지만, 배열이 null이거나 비어있는 것 역시 ServerException 발생
-        } else {
-            // [문제점] 37 Line의 WeatherDto[] weatherArray = responseEntity.getBody(); 는
-            //        계속 필요하지 않다가 47 Line 인 여기서부터 필요하다, 굳이 저 위에서 선언할 필요가 없다.
-            // [해결방안] 위 코드를 이쪽으로 옮김과 동시에 불필요한 else문을 제거한다
-            if (weatherArray == null || weatherArray.length == 0) {
-                throw new ServerException("날씨 데이터가 없습니다.");
-            }
         }
+
+        // 위에서 할당받은 값(responseEntity)의 Body 만을 WeatherDto 배열에 할당하기
+        // 위에서 할당받은 값의 상태코드가 정상이긴하지만, 배열이 null이거나 비어있는 것 역시 ServerException 발생
+        WeatherDto[] weatherArray = responseEntity.getBody();
+        if (weatherArray == null || weatherArray.length == 0) {
+            throw new ServerException("날씨 데이터가 없습니다.");
+        }
+
+        // [문제점] 37 Line의 WeatherDto[] weatherArray = responseEntity.getBody(); 는
+        //        계속 필요하지 않다가 47 Line 인 여기서부터 필요하다, 굳이 저 위에서 선언할 필요가 없다.
+        // [해결방안] 위 코드를 이쪽으로 옮김과 동시에 불필요한 else문을 제거하고, 분리한다
+
 
         // 값을 정상적으로 받아온 경우, 오늘 날짜(getCurrentDate())를 문자열에 할당
         String today = getCurrentDate();
